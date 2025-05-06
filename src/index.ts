ad27df3bac7bb9580d55897980963eaccd7da478
Eugene Koshelev <renderlife@mail.ru>
2025-05-06 22:27:52 +0300

    Доработка ci/cd и приложения

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,22 @@ import cors from "cors";
 
 const app = express();
 const PORT = 3002;
-//const FRONT_PORT = 3001;
+
+// Проверяем, что находимся в режиме разработки
+const isDev = process.env.NODE_ENV === "development";
+
+// CORS применяем только в dev-режиме
+if (isDev) {
+    app.use(cors({
+        origin: `http://localhost:3000`,
+    }));
+}
 
 app.use(bodyParser.json());
 app.use("/todos", todoRoutes);
 
-// app.use(cors({
-//     origin: `http://localhost:${FRONT_PORT}`,
-// }));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`NODE_ENV: ${process.env.NODE_ENV || "production"}`); // Для отладки
 });
