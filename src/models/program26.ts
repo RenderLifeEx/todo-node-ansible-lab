1f34344926524773f5bce08dc9930c181d880718
Eugene Koshelev <renderlife@mail.ru>
2026-08-19 12:55:54 +0300

    Добавил программы

--- src/models/program26.ts
+++ src/models/program26.ts
@@ -9,5 +9,15 @@ export const programs: Program[] = [
         name: 'Тестовая программа',
         url: 'https://todo-jq.renderlife.ru/sirius-navigator.html',
         lastStatus: 'unavailable'
-    }
+    },
+    {
+        name: 'Плавание',
+        url: 'https://do.sirius-ft.ru/program/1245-plavanie-obshcherazvivayushchaya-programma',
+        lastStatus: 'unavailable'
+    },
+    {
+        name: 'Мастерская Художника',
+        url: 'https://do.sirius-ft.ru/program/758-klub-zhivopisi',
+        lastStatus: 'unavailable'
+    },
 ];
